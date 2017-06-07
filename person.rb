82ee9b791a1cff76ac8fe2d9e64798cc7919e18d
Sjones352 <sjones@tistatech.com>
2017-06-07 15:35:26 -0400

    Fixed all Lint errors

--- person.rb
+++ person.rb
@@ -1,18 +1,21 @@
 require 'logger'
 
+# This is a class method that describes the name,
+# gender and location of person
 class Person
-	def initialize(name, gender)
-		@logger = Logger.new($stdout)
-		@name = name
-		@location = "Home"
-		@gender = "female"
-		@logger.debug ("Person:new instance created")
-	end
-	attr_accessor :location, :name, :gender
+  attr_accessor :location, :name, :gender
 
-	def say_hi
-		puts "My name is #{name}. Iam at #{location} and I am #{gender}. Hello"
-	end
+  def initialize(name, _gender)
+    @logger = Logger.new($stdout)
+    @name = name
+    @location = 'Home'
+    @gender = 'female'
+    @logger.debug('Person:new instance created')
+  end
+
+  def say_hi
+    puts "My name is #{name}. Iam at #{location} and I am #{gender}. Hello"
+  end
 end
 sandra = Person.new('diana', 'female')
 sandra.say_hi
