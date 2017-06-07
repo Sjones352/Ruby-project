a553000cf9ff993c47704f198924e8d4d74c5fcb
Sjones352 <sjones@tistatech.com>
2017-06-06 21:45:33 -0400

    Fixed indentation errors

--- person.rb
+++ person.rb
@@ -1,18 +1,19 @@
 require 'logger'
 
 class Person
-	def initialize(name, gender)
-		@logger = Logger.new($stdout)
-		@name = name
-		@location = "Home"
-		@gender = "female"
-		@logger.debug ("Person:new instance created")
-	end
-	attr_accessor :location, :name, :gender
+  attr_accessor :location, :name, :gender
 
-	def say_hi
-		puts "My name is #{name}. Iam at #{location} and I am #{gender}. Hello"
-	end
+  def initialize(name, gender)
+    @logger = Logger.new($stdout)
+    @name = name
+    @location = "Home"
+    @gender = "female"
+    @logger.debug ("Person:new instance created")
+  end
+
+  def say_hi
+    puts "My name is #{name}. Iam at #{location} and I am #{gender}. Hello"
+  end
 end
 sandra = Person.new('diana', 'female')
 sandra.say_hi
