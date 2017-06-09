d4b41078a48a064556662abff10530d9c691cf3c
Sjones352 <sjones@tistatech.com>
2017-06-08 22:49:06 -0400

    added Programmer class as a subclass of person

--- person.rb
+++ person.rb
@@ -17,5 +17,17 @@ class Person
     puts "My name is #{name}. Iam at #{location} and I am #{gender}. Hello"
   end
 end
+
+# Inheritance is a relation between two classes. The programmer class inherits
+# from the person class therefore getting its features. In short programmer
+# inherits the behaviour from person class.
+class Programmer < Person
+  def program(other_name)
+    puts "sup, I am #{name}, and I am working, leave me alone #{other_name}"
+  end
+end
+
 sandra = Person.new('diana', 'female')
 sandra.say_hi
+jones = Programmer.new('sandra', 'female')
+jones.program('max')
