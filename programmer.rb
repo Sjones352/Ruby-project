78005fc9c4380e8df61e6350c0828fedbe263b49
Sjones352 <sjones@tistatech.com>
2017-06-19 16:38:00 -0400

    validate method

--- programmer.rb
+++ programmer.rb
@@ -6,8 +6,16 @@ require './person.rb'
 # inherits the behaviour from person class.
 class Programmer < Person
   def program(other_name)
+    validate(other_name)
     puts "Hi, I am #{@name} and I am #{@gender}, and I am working. 
-    I am going to be an awesome software engineer,#{other_name.}!"
+    I am going to be an awesome software engineer,#{other_name}!"
+  end
+
+  def validate(other_name)
+    if @name.nil? || @gender.nil? || other_name.nil?
+      puts "Wrong, I am #{@name} and I am #{@gender}, and I am working. 
+      I am going to be an awesome software engineer,#{other_name}!"
+    end
   end
 end
 
