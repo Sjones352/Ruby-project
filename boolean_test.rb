722be7033dc9282751989427ab56ed2fb6d6077c
Sjones352 <sjones@tistatech.com>
2017-07-07 21:50:25 -0400

    boolean statements

--- boolean_test.rb
+++ boolean_test.rb
@@ -6,20 +6,22 @@
     #      output a message that contains a concatenation of all the messes in the array
     #      exit with error
 
-errors = []
 
+  class Print 
 
-name = ARGV[0]
-name_1 = ARGV[1]
-name_2 = ARGV[2]
-
-
-errors.push("err1") if !! name == name_1
-errors.push("err2") if !(name_1 != name_2)
-errors.push("err3") if (name == name_1) 
-
-if !errors.empty?
-  warn "Errors found"
-  warn errors.join("\n")
-  exit (1)
+  def self.disp(boolean_1, boolean_2)
+    if boolean_1 == boolean_2
+    puts "Missing boolean value 1"
+    puts "Missing boolean value 2"
+   elsif (boolean_1.to_s !=  boolean_2.to_s )
+    warn "#{boolean_1} is not a valid boolean value"
+    warn "#{boolean_2} is not a valid boolean value"
+    elsif boolean1.nil?
+      warn "#{boolean_1} is not a valid boolean value"
+    else
+        puts "you are very wrong"
+    end
+  end
 end
+
+Print.disp(ARGV[0], ARGV[1])
