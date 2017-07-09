437e1dbf9ba12988aa0355bffdfcdbe78dbddbae
Sjones352 <sjones@tistatech.com>
2017-07-08 20:29:52 -0400

    boolean values

--- boolean_test.rb
+++ boolean_test.rb
@@ -6,20 +6,35 @@
     #      output a message that contains a concatenation of all the messes in the array
     #      exit with error
 
-errors = []
 
 
-name = ARGV[0]
-name_1 = ARGV[1]
-name_2 = ARGV[2]
+class Print 
+  def self.disp(boolean1, boolean2)
+  	puts "Boolean 1: #{boolean1}"
+  	puts "Boolean 2: #{boolean2}"
+    
+    errors = []
 
+    if boolean1.nil?
+    	errors.push("Missing boolean 1")
+    elsif boolean1.to_s != "true" && boolean1.to_s != "false"
+    	errors.push("'#{boolean1}' is not true or false")
+    end
 
-errors.push("err1") if !! name == name_1
-errors.push("err2") if !(name_1 != name_2)
-errors.push("err3") if (name == name_1) 
+    if boolean2.nil?
+    	errors.push("Missing boolean 2")
+    elsif boolean2.to_s != "true" && boolean2.to_s != "false"
+    	errors.push("'#{boolean2}' is not true or false")
+    end
 
-if !errors.empty?
-  warn "Errors found"
-  warn errors.join("\n")
-  exit (1)
+    if !errors.empty?
+      warn "Errors encountered:\n" + errors.join("\n")
+      exit 1
+    end
+
+  end
 end
+
+Print.disp(ARGV[0], ARGV[1])
+
+
