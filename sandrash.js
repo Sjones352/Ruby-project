d9955861a3197444b0868d94e77a36f35e5e0c7b
Sjones352 <sjones@tistatech.com>
2017-08-12 23:43:29 -0400

    made changes to readline file

--- sandrash.js
+++ sandrash.js
@@ -2,46 +2,31 @@
 
 const readline = require('readline');
 
-//instance of readline
 const rl = readline.createInterface(process.stdin, process.stdout); 
 
-// object are also values. The values are written as name value pairs separated by a :
-var person = {
-	firstname: '',
-    lastname: '' };
+rl.question('Enter your first name> ', function(firstName) {
 
-// Set the First name. question method asks user to enter first name
-rl.question('Enter your first name >', function(names) {
- 
-person.names = names.trim();
+rl.setPrompt(`[${firstName.trim()}] sandrash$ `);
 
-rl.setPrompt(`[${person.names}] sandrash$ `);
+rl.prompt();
 
- // Readies realine for input from user, putting current setPrompt options to new line
-rl.prompt(); 
-
-// This is an event. Listening for new line. Usually recieved when user hits enter.
 rl.on('line', function(answer) {
-   
-  answer = answer.toString().trim();
-  var result = (answer === 'true' || answer === 'false');
- 
-  if (!answer || answer === '') {
-     console.log(`You gave me nothing `);
+    
+  if (!answer) {
+    console.log('You gave me nothing');
    } else if (!isNaN(Number.parseInt(answer))) {
-     console.log(`you entered the integer is ${answer} `);
-   } else if (result) {
-     console.log(`You entered the boolean ${answer} `);
-   }else if (answer === 'exit') {
-   // closes instance, relinguishing control on the input and output	
+    console.log('You entered the integer', answer);
+   } else if (answer === 'true' || answer === 'false') {
+    console.log('You entered the boolean value', answer);
+   } else if (answer === 'exit') {
    rl.close(); 
   } 
   rl.prompt();
-  });
+ });
 });
-// Listening for close
+
 rl.on('close', function () {
 	console.log('Goodbye!');
-    process.exit(0);
+  process.exit(0);
 });
 
