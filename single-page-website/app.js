2352a591ece03c3928e96bc4291b450ea53825b7
Sjones352 <sjones@tistatech.com>
2017-09-03 02:28:09 -0400

    new website

--- single-page-website/app.js
+++ single-page-website/app.js
@@ -1,6 +1,14 @@
 
-window.onload = function() {
-  document.getElementById('hello').onclick = function(e) {
-    alert('hello');
-  };
-};
+// window.onload = function() {
+//   document.getElementById('login').onclick = function check(form) {
+//     if (form.userid.value == 'sandra' && form.pswrd.value = '123')
+//     	window.open("https://www.w3schools.com/TAgs/tryit.asp?filename=tryhtml_body_background)
+//      }
+//      else{
+//      	alert("The username and password doesn't match")
+//      }
+//   };
+// };
+
+
+// $('navbar-head').localScroll();
