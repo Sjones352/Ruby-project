acbff64dde8d337992b027b838c12f233172a886
Sjones352 <sjones@tistatech.com>
2017-09-05 00:59:09 -0400

    added logo to page

--- single-page-website/app.js
+++ single-page-website/app.js
@@ -12,3 +12,12 @@
 
 
 // $('navbar-head').localScroll();
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
